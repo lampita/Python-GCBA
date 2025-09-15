a462fd31f95676a4cfad15343141ce4922f90fed
Jose Lamparelli <jose.lamparelli@gmail.com>
2025-09-15 09:56:40 -0300

    terminados logica caso 1 y 2

--- clase_7.py
+++ clase_7.py
@@ -1,12 +1,25 @@
-"""Guarde cada nombre válido en una lista, asegurándote de agregarlo con el método .append().
-
-Permití que se finalice la carga de nombres escribiendo la palabra "fin".
-
+"""
 Una vez finalizada la carga, ordená alfabéticamente los nombres en la lista y mostrá la lista ordenada utilizando un bucle for."""
 
 import os, datetime
 
-lista_de_nombres: []
+lista_de_nombres=[]
+
+def nombre_es_valido(nombre):
+    if not nombre.strip():
+        
+        return "\n❌ Error. ->Campo Vacío",False
+     
+    if any(char.isdigit() for char in nombre):
+        return "\n❌ Error. ->Contiene Números", False
+    
+    if any(not char.isalpha() and not char.isspace() for char in nombre):
+        return "\n❌ Error. ->Contiene caracteres especiales o inválidos", False
+    
+    return " ✔️\n",True
+
+
+
 
 salir = False
 while not salir:
@@ -15,7 +28,7 @@ while not salir:
         os.system("cls")
     else:
         os.system("clear")
-    print(f"\t{'=' * 35}\n\t\033[1;91mINGRESO DE NOMBRES\033[0m\n\t{'=' * 35}")
+    print(f"\t{'=' * 19}\n\t\033[1;91mINGRESO DE NOMBRES\033[0m\n\t{'=' * 19}")
 
     print("""MENU DE OPCIONES:
     1. Ingresar Nombre y Apellido
@@ -24,18 +37,49 @@ while not salir:
     4. Ordenar por Nombre
     5. Salir
     """)
-    print(datetime.date.today())
-    opcion_menu = input("Ingresar opcion:")
+    
+    opcion_menu = input("OPCION:  ")
     match opcion_menu:
         case "1":
-            continue
+            print("\nINGRESANDO DATOS (Sale con \033[1;91m'fin'\033[0m.)")     
+            while True:
+                nombre = input("INGRESE NOMBRE: ").strip()
+                if nombre.lower() == 'fin':
+                    break
+
+                if not (nombre_es_valido(nombre)[1]):
+                    
+                    print(f"{(nombre_es_valido(nombre)[0])}. ->Intente Nuevamente.")
+                    continue
+                else:
+                    print(nombre,nombre_es_valido(nombre)[0])    
+                
+                apellido = input("INGRESE APELLIDO: ").strip()
+                if apellido.lower() == 'fin':
+                    break
+
+                if not nombre_es_valido(apellido):
+                    print(f"{(nombre_es_valido(apellido)[0])}. ->Intente Nuevamente.")
+                    continue
+                else:
+                    print(apellido, nombre_es_valido(apellido)[0])    
+                nombre_completo = f"{nombre.title()} {apellido.title()}"
+                lista_de_nombres.append(f"{datetime.datetime.now().strftime("%d/%m/%Y")} {nombre_completo}")
+                print(f"{datetime.datetime.now().strftime("%d/%m/%Y")}  '{nombre_completo}' ->Agregado a la Lista.")
 
         case "2":
-            continue
+            if not lista_de_nombres:
+                print("La lista de nombres está vacía.")
+            else:
+                print("\nMOSTRANDO LISTA COMPLETA:\n")
+                for i, nombre_completo in enumerate(lista_de_nombres, start=1):
+                    print(f"{i}. {nombre_completo}")
+            input("\nPresione ENTER para volver al menú...")
 
         case "3":
-            continue
+            ...
         case "4":
-            continue
+            ...
         case "5":
+            print("Saliendo...\n\n")
             salir = True
--- prueba.py
+++ prueba.py
@@ -1,12 +1,19 @@
-import sys
+def nombre_es_valido(nombre):
+    if not nombre.strip():
+        print ("Campo vacío")
+        return "campo vacio",False
+     
+    if any(char.isdigit() for char in nombre):
+        print ("Contiene números")
+        return False
+    
+    if any(not char.isalpha() for char in nombre):
+        print("Contiene caracteres especiales")
+        return False
+    
+    return True
 
-# Escribir el mensaje sin un salto de línea al final
-sys.stdout.write("Ingresa tu nombre: ")
+nombre=""
 
-# Asegurar que el mensaje se muestre inmediatamente
-sys.stdout.flush()
 
-# Capturar la entrada del usuario
-nombre = sys.stdin.readline().strip()
-
-print(f"\rHola, {nombre}!")
+print(nombre_es_valido(nombre))
