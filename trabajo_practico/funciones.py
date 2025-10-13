bd81ad99caa7bd25d6679ab19223f8a5eb656ef3
Jose Lamparelli <jose.lamparelli@gmail.com>
2025-10-13 07:00:16 -0300

    Logica agregada a Case 5 y 6

--- trabajo_practico/funciones.py
+++ trabajo_practico/funciones.py
@@ -2,14 +2,18 @@ import os
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
-#console = Console(highlight=False)
-#umbral_stock_critico = 50
-#umbral_vencimiento_critico = 5
+from rich.style import Style
+from datetime import datetime, timedelta
+
+hoy = datetime.now().date()
+
+
 def borrar_consola():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
+
 
 def conv_to_super(n):
     super = list(map(chr, [8304, 185, 178, 179, 8308, 8309, 8310, 8311, 8312, 8313]))
@@ -19,86 +23,110 @@ def conv_to_super(n):
     return st
 
 
-
-
-
-def crear_tabla(resultados, query="LOTE"):
-
+def num_es_valido(num):
+    if not num.strip():
+        return (" ->Dato Vacío -> No se modificó el valor.\n"), "❌ "
+    if not num.isdigit():  ##isdigt() tambien devuelve False en casos de str que representen numeros negativos
+        return (" ->Dato Erroneo -> No se modificó el valor.\n"), "❌ "
+    if int(num) < 0:
+        return (" ->Numero Negativo-> No se modificó el valor.\n"), "❌ "
+    else:
+        return int(num), "✔️\n"
     
-    table = Table(title="Busqueda. Palabra clave: " + query)
-    table.add_column("Lote", justify="left")
+def float_es_valido(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+def validar_fecha(cadena_fecha):
+    
+    try:
+        datetime.strptime(cadena_fecha, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+
+
+def agregar_ultima_clave(clave):
+    penultimo_lote = []
+    for caracter in clave[::-1]:
+        if caracter.isdigit():
+            penultimo_lote.append(caracter)
+        else:
+            break
+    penultimo_lote.reverse()
+    ultima_clave = "".join(penultimo_lote)
+    ultima_clave = str(int(ultima_clave) + 1)
+    return "LOTE-" + ultima_clave
+
+
+def crear_tabla(resultados, titulo, query=None):
+    table = Table(title=titulo)
+    table.add_column("Lote", justify="left", style="orange3 bold")
     table.add_column("SKU", justify="left")
     table.add_column("Producto", justify="left")
     table.add_column("Marca", justify="left")
-    table.add_column("Comprado", justify="right")
     table.add_column("Origen", justify="left")
     table.add_column("Vence", justify="right")
     table.add_column("Stock", justify="right")
     table.add_column("Precio", justify="right")
     table.add_column("Descripción", justify="left")
 
-
     for resultado in resultados:
-
-        
-        
-            
         lote = Text(str(resultado[0][1]))
-        lote.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            lote.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         sku = Text(str(resultado[0][0]))
-        sku.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            sku.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         producto = Text(str(resultado[1]["producto"]))
-        producto.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            producto.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         marca = Text(str(resultado[1]["nombre_fantasia"]))
-        marca.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
-
-        comprado = Text(str(resultado[1]["cantidad_unidades_en_stock"]))
-        comprado.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            marca.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         origen = Text(str(resultado[1]["pais_de_origen"]))
-        origen.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            origen.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         vence = Text(str(resultado[1]["fecha_de_vencimiento"]))
-        vence.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            vence.highlight_words([query], style="bold yellow", case_sensitive=False)
+        elif (
+            resultado[1]["fecha_de_vencimiento"] != "N/A"
+            and hoy
+            > datetime.strptime(resultado[1]["fecha_de_vencimiento"], "%Y-%m-%d").date()
+        ):
+            vence.style = "red bold"
 
         stock = Text(str(resultado[1]["cantidad_unidades_en_stock"]))
-        stock.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            stock.highlight_words([query], style="bold yellow", case_sensitive=False)
+        elif resultado[1]["cantidad_unidades_en_stock"] == 0:
+            stock.style = "red bold"
 
         precio = Text(str(resultado[1]["precio"]))
-        precio.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            precio.highlight_words([query], style="bold yellow", case_sensitive=False)
 
         descripcion = Text(str(resultado[1]["pequena_descripcion"]))
-        descripcion.highlight_words(
-            [query], style="bold yellow", case_sensitive=False
-        )
+        if query is not None:
+            descripcion.highlight_words(
+                [query], style="bold yellow", case_sensitive=False
+            )
 
         table.add_row(
             lote,
             sku,
             producto,
             marca,
-            comprado,
             origen,
             vence,
             stock,
@@ -106,5 +134,4 @@ def crear_tabla(resultados, query="LOTE"):
             descripcion,
         )
     return table
-
 
